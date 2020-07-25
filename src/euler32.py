e33f255f1a3eca29c0ce297be452993c8cda2b05
Jerry Tann <jerry.tann@optize.cn>
2020-07-25 21:28:51 +0800

    feat: add reverseSlice method

--- src/euler32.py
+++ src/euler32.py
@@ -6,6 +6,24 @@ def swap(arr, idx1, idx2):
   arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
   return arr
 
+def reverseSlice(arr, idx1, idx2):
+  sliced = arr[idx1:idx2+1]
+  prefix = arr[0:idx1]
+  reversedSlice = sliced[::-1]
+  return prefix + reversedSlice
+
+def getNextPermutation(input):
+  ret = input[:]
+  for i in range(len(ret) - 1, -1, -1):
+    cur = ret[i]
+    print('cur is ', cur)
+    for j in range(i - 1, -1, -1):
+      comp = ret[j]
+      if j < i:
+        ret = swap(ret[:], i, j)
+
+  return ret
+
 testBase = [1,2,3,4,5]
 testSwap = copy.copy(testBase)
 res = swap(testSwap, 0,4)
@@ -14,7 +32,10 @@ reverseRes = testReverse[::-1]
 testSlice = testBase[:]
 sliced = testBase[2:4]
 joined = testBase[0:2] + testBase[2:5]
-print('reverseRes is ', reverseRes)
-print('sliced is ', sliced)
-print('joined is ', joined)
-print('testBase is ', testBase)
+
+
+testPerm = [1,2,3,4,5]
+reversedSlice = reverseSlice(testPerm[:], 3, 4)
+nextPerm = getNextPermutation(testPerm)
+print('nextPerm is ', nextPerm)
+print('reverseSlice is ', reversedSlice)
