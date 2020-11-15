cc117b1f8e1fec07d840a766bb489b50c8cf3964
Raskolnikov <jerry.tann@opteyes.cn>
2020-11-15 17:05:22 +0800

    feat: euler 38 solution (partial)

---
+++ src/euler38.py
@@ -0,0 +1,34 @@
+print('project euler problem 38')
+
+# min n 2
+# max x = 987654321/2
+# max n = 9
+
+# formula 
+
+
+def isPandigital(inputN):
+  asStr = str(inputN)
+  if len(asStr) != 9:
+    return False
+  panSet = set("123456789")
+  return len(panSet.intersection(set(asStr))) == 9
+
+def applyFormula(x, n):
+  sumR = ""
+  for i in range(1, n+1):
+    sumR += str(x*i)
+  return sumR
+
+print("9, 5", applyFormula(9, 5))
+
+maxX = 987654321 // 3
+for x in range (2, maxX):
+  for n in range (2, 5):
+    sumR = applyFormula(x, n)
+    if isPandigital(sumR):
+      print("pan {} {} {}".format(x, n, sumR))
+
+# tests 
+
+
