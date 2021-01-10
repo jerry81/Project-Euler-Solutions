5a9a7f5f63e84546dd4922864da3aef5a131168a
Raskolnikov <jerry.tann@opteyes.cn>
2021-01-10 16:39:56 +0800

    feat: filter list down ( a bit )

--- src/euler51.py
+++ src/euler51.py
@@ -1,5 +1,6 @@
 import time 
 from utils.mathHelpers import eratosthenes
+from utils.mathHelpers import isPrime
 print('project euler problem 51')
 tic = time.perf_counter()
 
@@ -8,6 +9,24 @@ f = open("./resources/primesTo10000000.txt", "r")
 asStr = f.read()
 asList = asStr.split(',')
 asIntList = [int(item) for item in asList]
-print(asIntList)
+
+# filter list down to items with repeats
+def hasDupe(input):
+  asStr = str(input)
+  hasDupe = False
+  countMap = {}
+  for i in range(0, len(asStr)):
+    countMap[asStr[i]] = 0
+  for i in range(0, len(asStr)):
+    countMap[asStr[i]] += 1
+    if countMap[asStr[i]] > 1:
+      hasDupe = True
+  return not hasDupe
+
+noDupes = list(filter(hasDupe,asIntList))
+
+print("len with dupes is ", len(asIntList))
+print("no dupes is ", noDupes)
+print("len is ", len(noDupes))
 toc = time.perf_counter()
 print('finished in ', toc - tic)
