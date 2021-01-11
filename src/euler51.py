8b9c374e186bfed0642948e0def0e1f8413b3d8e
Raskolnikov <jerry.tann@opteyes.cn>
2021-01-11 12:21:00 +0800

    feat: add method to pick out repeated indexes to try in brute force

--- src/euler51.py
+++ src/euler51.py
@@ -5,7 +5,7 @@ print('project euler problem 51')
 tic = time.perf_counter()
 
 #read the file:
-f = open("./resources/primesTo10000000.txt", "r")
+f = open("./resources/nonDupesFiltered.txt", "r")
 asStr = f.read()
 asList = asStr.split(',')
 asIntList = [int(item) for item in asList]
@@ -23,11 +23,41 @@ def hasDupe(input):
       hasDupe = True
   return hasDupe
 
-noDupes = list(filter(hasDupe,asIntList))
+""" noDupes = list(filter(hasDupe,asIntList)) """
 
 """ f = open("./resources/nonDupesFiltered.txt", "x+")
 f.write(",".join([str(elem) for elem in noDupes]))
 f.close() """
 
+fiveDigitPrimes = list(filter(lambda x: len(str(x)) == 5,asIntList))
+sixDigitPrimes = list(filter(lambda x: len(str(x)) == 6,asIntList))
+print('len is ', len(fiveDigitPrimes))
+print('len6 is ', len(sixDigitPrimes))
+
+# brute force: 
+# step thru each permutation of swapped indexes, index 1, index 2... index 1 and 2, index 1 and 3 ... 
+# fix some digits swap some digits.  then for each 
+# fixed number and idx pair, try 0-9 (10 ops), for each item that matches, add 1 to the count
+# permutations of abcde - ab ac ad ae
+
+def makeRepeatIndexSet(inputArr):
+  repeats = set()
+  for i in range(0, len(inputArr)):
+    repeat = []
+    cur = str(inputArr[i])
+    for j in range(0, len(cur)):
+      for k in range(j, len(cur)):
+        if cur[j] == cur[k]: 
+          repeat.append(j)
+          repeat.append(k)
+      repeats.add(tuple((set(repeat))))
+  return repeats
+    
+print('99991', isPrime(99991))
+print('99989', isPrime(99989))
+
+repeats = makeRepeatIndexSet(fiveDigitPrimes)
+print('repeats are ', repeats)
+# print('five', fiveDigitPrimes)
 toc = time.perf_counter()
 print('finished in ', toc - tic)
