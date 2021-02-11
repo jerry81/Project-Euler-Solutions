2cd12b5e3f56921af8554a904355f9a61ff0797e
Raskolnikov <jerry.tann@opteyes.cn>
2021-02-11 11:53:46 +0800

    feat: more impl of euler 19 (wip)

--- src/euler19.py
+++ src/euler19.py
@@ -2,11 +2,18 @@ print('project euler problem 19')
 
 def isLeapYear(year):
         return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+def getNextDay(day):
+        if day == 7:
+                day = 1
+        else:
+                day += 1
+        return day
 
 def nextDay(curDay):
         month = curDay.month
         day = curDay.day
         year = curDay.year
+        dayOfWeek = 1
         leap = isLeapYear(year)
         nextDay = { day: -1, month: -1, year: -1 }
         if month == 2:
@@ -22,6 +29,18 @@ def nextDay(curDay):
                                 day = 1
                         else:
                                 day += 1
+        elif month in [9, 4, 6, 11]:
+                if day == 30:
+                        month += 1
+                        day = 1
+                else:
+                        day += 1
+        elif month == 12: 
+                if day == 31:
+                        month = 1
+                        day = 1
+                        year += 1
+
 
 dayOfWeek = 0
 curMonth = 1
@@ -29,10 +48,5 @@ curMonth = 1
 You are given the following information, but you may prefer to do some research for yourself.
 
 1 Jan 1900 was a Monday.
-Thirty days has September,
-April, June and November.
-All the rest have thirty-one,
-Saving February alone,
-Which has twenty-eight, rain or shine.
 And on leap years, twenty-nine.
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
