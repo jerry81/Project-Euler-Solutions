0c21b4b250428c8f3bdccc29329269a1a3d5d13b
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-25 15:02:56 +0800

    feat: try some optimizations

--- src/euler51.py
+++ src/euler51.py
@@ -1,36 +1,40 @@
-import time 
+import time
 from utils.mathHelpers import eratosthenes
 from utils.mathHelpers import isPrime
 print('project euler problem 51')
 tic = time.perf_counter()
 
-#read the file:
-""" f = open("./resources/nonDupesFiltered.txt", "r")
+# read the file:
+f = open("./resources/nonDupesFiltered.txt", "r")
 asStr = f.read()
 asList = asStr.split(',')
-asIntList = [int(item) for item in asList] """
+asIntList = [int(item) for item in asList]
 
 # filter list down to items with repeats
-def hasDupe(input):
-  asStr = str(input)
-  hasDupe = False
-  countMap = {}
-  for i in range(0, len(asStr)):
-    countMap[asStr[i]] = 0
-  for i in range(0, len(asStr)):
-    countMap[asStr[i]] += 1
-    if countMap[asStr[i]] > 1:
-      hasDupe = True
-  return hasDupe
 
-""" noDupes = list(filter(hasDupe,asIntList)) """
+
+def hasDupe(input):
+    asStr = str(input)
+    hasDupe = False
+    countMap = {}
+    for i in range(0, len(asStr)):
+        countMap[asStr[i]] = 0
+    for i in range(0, len(asStr)):
+        countMap[asStr[i]] += 1
+        if countMap[asStr[i]] > 1:
+            hasDupe = True
+    return hasDupe
+
+# noDupes = list(filter(hasDupe,asIntList)) """
+
 
 """ f = open("./resources/nonDupesFiltered.txt", "x+")
 f.write(",".join([str(elem) for elem in noDupes]))
 f.close() """
 
-# fiveDigitPrimes = list(filter(lambda x: len(str(x)) == 5,asIntList))
-# sixDigitPrimes = list(filter(lambda x: len(str(x)) == 6,asIntList))
+fiveDigitPrimes = list(filter(lambda x: len(str(x)) == 5, asIntList))
+
+sixDigitPrimes = list(filter(lambda x: len(str(x)) == 6,asIntList))
 """ sevDigitPrimes = list(filter(lambda x: len(str(x)) == 7, asIntList))
 f = open("./resources/sevenDigitPrimes.txt", "x+")
 f.write(",".join([str(elem) for elem in sevDigitPrimes]))
@@ -38,48 +42,68 @@ f.close()  """
 f = open("./resources/sevenDigitPrimes.txt", "r")
 asStr = f.read()
 asList = asStr.split(',')
-sevDigitPrimes = [int(item) for item in asList] 
+sevDigitPrimes = [int(item) for item in asList]
 print('sev len is ', len(sevDigitPrimes))
 # print('len is ', len(fiveDigitPrimes))
 # print('len6 is ', len(sixDigitPrimes))
 
-def getFamily(replacedDigits, baseNumber):
+
+def getFamily(replacedDigits, baseNumber, maxNonPrimes):
   bnAsStr = list(str(baseNumber))
+  nonPrimeCount = 0
   family = []
   for i in range(0, 10):
     for dig in replacedDigits:
       bnAsStr[dig] = str(i)
-    family.append(int(''.join(bnAsStr)))
+    if not isPrime(int(''.join(bnAsStr))):
+      nonPrimeCount += 1
+      if nonPrimeCount > maxNonPrimes:
+        return []
+    else: 
+      family.append(int(''.join(bnAsStr)))
   return family
 
-# brute force: 
-# step thru each permutation of swapped indexes, index 1, index 2... index 1 and 2, index 1 and 3 ... 
-# fix some digits swap some digits.  then for each 
+print("isPrime", not isPrime(86507))
+# brute force:
+# step thru each permutation of swapped indexes, index 1, index 2... index 1 and 2, index 1 and 3 ...
+# fix some digits swap some digits.  then for each
 # fixed number and idx pair, try 0-9 (10 ops), for each item that matches, add 1 to the count
 # permutations of abcde - ab ac ad ae
 
+
 def getPossibleReplacements(max):
-  replacementIndexes = []
-  for i in range(0, max):
-    for j in range(i, max):
-      newList = [i, j]
-      replacementIndexes.append(newList)
-  return replacementIndexes
+    replacementIndexes = []
+    for i in range(1, max):
+        for j in range(i, max):
+            newList = [i, j]
+            replacementIndexes.append(newList)
+    return replacementIndexes
 
 
 def makeRepeatIndexSet(inputArr):
-  repeats = set()
-  for i in range(0, len(inputArr)):
-    repeat = []
-    cur = str(inputArr[i])
-    for j in range(0, len(cur)):
-      for k in range(j, len(cur)):
-        if cur[j] == cur[k]: 
-          repeat.append(j)
-          repeat.append(k)
-      repeats.add(tuple((set(repeat))))
-  return repeats
-    
+    repeats = set()
+    for i in range(0, len(inputArr)):
+        repeat = []
+        cur = str(inputArr[i])
+        for j in range(0, len(cur)):
+            for k in range(j, len(cur)):
+                if cur[j] == cur[k]:
+                    repeat.append(j)
+                    repeat.append(k)
+            repeats.add(tuple((set(repeat))))
+    return repeats
+
+replacements = getPossibleReplacements(7)
+print('replacements is ', replacements)
+for pri in sevDigitPrimes:
+    for replacement in replacements:
+        family = getFamily(replacement, pri, 2)
+       # print('family possibility is ', family)
+        if (len(family) == 8):
+            print("family is ", family)
+
+
+
 print('99991', isPrime(99991))
 print('99989', isPrime(99989))
 
@@ -89,10 +113,10 @@ print('99989', isPrime(99989))
 
 replacements = getPossibleReplacements(7)
 for pri in sevDigitPrimes:
-  for replacement in replacements:
-    family = getFamily(replacement, pri)
-    if (len(family) == 8):
-      print("family is ", family)
+    for replacement in replacements:
+        family = getFamily(replacement, pri)
+        if (len(family) == 8):
+            print("family is ", family)
 # print('five', fiveDigitPrimes)
 toc = time.perf_counter()
 print('finished in ', toc - tic)
