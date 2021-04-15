e02e7374e3c6e184056b0232e444589d1fe12e8b
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-15 14:44:23 +0800

    feat: add info on functools.wraps

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,23 +1,30 @@
+import functools
+
+
 def my_dec(decorated):
-  def wrapper(*args, **kwargs):
-    print('pre')
-    decorated(*args, **kwargs)
-    print('post')
-  return wrapper
+    def wrapper(*args, **kwargs):
+        print('pre')
+        decorated(*args, **kwargs)
+        print('post')
+    return wrapper
+
 
 @my_dec
 def hello():
-  print('hello no arg')
+    print('hello no arg')
+
 
 @my_dec
 def hello2(item):
-  print('hello with item', item)
+    print('hello with item', item)
+
 
 @my_dec
 def helloWithMixed(item, kw1, kw2):
-  print("fixed is ", item)
-  print("kw1 is ", kw1)
-  print("kw2 is ", kw2)
+    print("fixed is ", item)
+    print("kw1 is ", kw1)
+    print("kw2 is ", kw2)
+
 
 hello()
 hello2("world")
@@ -25,18 +32,22 @@ helloWithMixed("fixed", kw2="kw2", kw1="kw1")
 
 # example of decorating with return value
 
+
 def my_dec2(fn):
-  def wrapper(*arg, **args):
-    print('pre')
-    returned = fn(*arg, **args)
-    print('post')
-    return returned
-  return wrapper
+    @functools.wraps(fn)  # lets fn keep its introspection info
+    def wrapper(*arg, **args):
+        print('pre')
+        returned = fn(*arg, **args)
+        print('post')
+        return returned
+    return wrapper
+
 
 @my_dec2
 def helloWithReturn(num):
-  print('working it arg is ', num)
-  return num+1
+    print('working it arg is ', num)
+    return num+1
+
 
 result = helloWithReturn(2)
 print("result is ", result)
