d7d10f04a65af6527840c5b30ec24b0cee396ec6
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-23 11:07:31 +0800

    feat: setup euler 54

---
+++ src/euler54.py
@@ -0,0 +1,18 @@
+from utils.annotations import track_performance
+import math
+
+print('project euler problem 54')
+
+CARD_FACES = range(13)
+
+class Card():
+  suit = "h" # s, c, d, h
+  value = 0 # 2
+
+def checkRoyal(cards):
+
+
+@track_performance
+def euler54():
+  print('start')
+euler54()
