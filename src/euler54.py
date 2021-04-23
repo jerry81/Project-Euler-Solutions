561b024fc808d6616081b9a7f1f7aa812a0a0e29
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-23 17:04:39 +0800

    feat: get 4 of a kind check

--- src/euler54.py
+++ src/euler54.py
@@ -14,6 +14,13 @@ class Card:
     self.value = v 
   def __str__(self):
     return f'suit {self.suit}, value {self.value}'
+
+class H2HResult:
+  rank: -1
+  high: -1
+  def __init__(self,r,h):
+    self.rank = r
+    self.high = h
   
 VALUE_MAP = {
   '2': 2,
@@ -90,6 +97,14 @@ def getFreqMap(cards):
       freqMap[val] = 1
   return freqMap
 
+def getFourOfAKind(cards):
+  freqMap = getFreqMap(cards)
+  for val, freq in freqMap.items():
+    if freq == 4:
+      return val
+  return -1
+
+
 def testSameSuit():
   cards1 = []
   cards1.append(Card('H', '2'))
@@ -144,6 +159,24 @@ def testFreqMap():
   print('freqMap 1, 7 high', getFreqMap(cards1))
   print('freqMap 2, 3 nines', getFreqMap(cards2))
 
+def test4():
+  cards1 = []
+  cards1.append(Card('H', '7'))
+  cards1.append(Card('H', '3'))
+  cards1.append(Card('H', '4'))
+  cards1.append(Card('H', '5'))
+  cards1.append(Card('H', '6'))
+  
+  cards2 = []
+  cards2.append(Card('H', '9'))
+  cards2.append(Card('S', '9'))
+  cards2.append(Card('C', '9'))
+  cards2.append(Card('H', '5'))
+  cards2.append(Card('D', '9'))
+
+  print('4oak 1, -1', getFourOfAKind(cards1))
+  print('4oak 2, 9', getFourOfAKind(cards2))
+
 @track_performance
 def euler54():
   cards = prepare()
@@ -151,3 +184,4 @@ euler54()
 testSameSuit()
 testStraightHigh()
 testFreqMap()
+test4()
