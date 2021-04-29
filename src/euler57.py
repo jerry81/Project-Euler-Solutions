6bd8ba711152ca61a605016a9704533c262b5964
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-29 11:40:35 +0800

    feat: test use denom

--- src/euler57.py
+++ src/euler57.py
@@ -8,6 +8,9 @@ def useDenom(target, num, denom = None):
     return Fraction(num * target, target)
   return Fraction(target * num, target * denom)
 
+def expDenom(cur):
+  return Fraction(1) / (Fraction(2) + cur)
+
 def longSqrt(base, expansions):
   pass
   # expansions = 4
@@ -21,9 +24,18 @@ def testUseDenom():
   pro = fraction1*fraction2
   print('4/4 is ', pro)
 
+def testExpDenom():
+  first = expDenom(Fraction(1, 2))
+  second = expDenom(first)
+  third = expDenom(second)
+  print('2/5 is ', first)
+  print('5/12 is ', second)
+  print('12/29 is ', third)
+
 @track_performance
 def euler57():
   print('57')
 
 euler57()
 testUseDenom()
+testExpDenom()
