0de70f7d027d298944df4b4725aecf8fed9787cb
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-30 18:30:18 +0800

    feat: get ready to make spirals

--- src/euler58.py
+++ src/euler58.py
@@ -2,6 +2,18 @@ from utils.annotations import track_performance
 
 print('project euler problem 58')
 
+# 1
+# 5 4 3
+# 6 1 2
+# 7 8 9 
+# 17 16 15 14 13
+# 18 5  4  3  12 
+# 19 6  1  2  11 
+# 20 7  8  9  10 
+# 21 22 23 24 25
+def makeSpirals(iterations):
+  pass
+
 @track_performance
 def euler58():
   print('spirals')
