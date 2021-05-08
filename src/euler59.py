5de94294741619ceeea595ae75aa7468ad108b53
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-08 13:33:28 +0800

    feat: tests for test array to binary and xor

--- src/euler59.py
+++ src/euler59.py
@@ -3,13 +3,15 @@ from utils.fileUtils import openAndSplit
 import binascii
 
 asArr = openAndSplit('./resources/p059_cipher.txt')
-print('asArr', asArr)
+asCharArr = list(map(lambda x: chr(int(x)), asArr))
+print('asArr', asCharArr)
 def stoa(s):
   charArr = list(s)
   return list(map(lambda x: ord(x),charArr))
 
 def atos(a):
   return list(map(lambda x: chr(x), a))
+
 # helper convert ascii to char
 
 # string to binary 
@@ -24,6 +26,13 @@ def atos(a):
 # helper to decrypt with a 3 letter key
 # loop through all 26 lowercase characters for strings of length 3 until a sensible message appears
 
+def testAtoB():
+  print('a to b a', "{0:b}".format((97)))
+  print('a to b b', "{0:b}".format(ord('b')))
+
+def testBtoA():
+  print('b to a a', chr(int('1100001', 2)))
+
 def testStoA():
   source = 'abc'
   print('to ascii is ', stoa(source))
@@ -36,10 +45,22 @@ def testAtoS():
   print('to char arr abc is ', atos(source))
   print('def is ', atos(source2))
 
+def testXOR():
+  s1 = 'a'
+  key1 = 'a'
+  expected = '0000000'
+  # print('xor should be {} - {}'.format(expected, bin(ord(s1)) ^ bin(ord(key1))))
+
 @track_performance
 def euler59():
   print('project euler problem 59')
 
+print('xor test 0bx0 is', bin(97 ^ 97))
+print('xor test 0bx11 is ', bin(97 ^ 98))
+
 euler59()
 testStoA()
 testAtoS()
+testAtoB()
+testBtoA()
+testXOR()
