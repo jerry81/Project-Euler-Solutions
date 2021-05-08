908bd3bd15394d7f26dc9c793b352df22bf77e6d
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-08 17:08:18 +0800

    feat: solution to euler 59

--- src/euler59.py
+++ src/euler59.py
@@ -48,6 +48,11 @@ def testXOR():
   s1 = 'a'
   key1 = 'a'
   expected = '0000000'
+  a = 65
+  b = 42
+  c = 107
+  print('a ^ b = c', a ^ b)
+  print('c ^ b = a', c ^ b)
   # print('xor should be {} - {}'.format(expected, bin(ord(s1)) ^ bin(ord(key1))))
 
 # key is array of ascii codes between a-z 97 to 123
@@ -59,23 +64,51 @@ def decypher(key):
     output.append(chr(decoded))
   return output
 
+def checkFirstWord(charArr):
+  for i in range(len(charArr)):
+    if not charArr[i].isalpha():
+      return charArr[0:i]
+
+def checkFirstNWords(charArr, n):
+  count = 0
+  for i in range(len(charArr)):
+    if not charArr[i].isalpha():
+      count += 1
+      if count >= n:
+        return charArr[0:i]
+
 def testDecypher():
-  print('output is ', decypher([97, 98, 99]))
+  arr = decypher([ord('e'), ord('x'), ord('p')])
+  asciArr = list(map(lambda x: ord(x), arr))
+  print('arr is ', asciArr)
+  print('sum is ', sum(asciArr))
 
 @track_performance
 def euler59():
   print('project euler problem 59')
-  for i in range(97, 124):
-    for j in range(97, 124):
-      for k in range(97, 124):
-        print(decypher([i,j,k]))
+  f2 = open("./resources/59_outputs.txt", "x+")
+  for i in range(97, 123):
+    for j in range(97, 123):
+      for k in range(97, 123):
+        ch = decypher([i,j,k])
+        firstWord = checkFirstWord(ch)
+        # if len(firstWord) == 1 and firstWord[0] != 'I' and firstWord[0] != 'A':
+        #  continue
+        if len(firstWord) == 0:
+          continue
+        threeWords = checkFirstNWords(ch, 20)
+        f2.write(''.join([str(chr(i)),str(chr(j)),str(chr(k))]))
+        f2.write('   ')
+        f2.write("".join(threeWords))
+        f2.write('\n')
+  f2.close()
 
 print('xor test 0bx0 is', bin(97 ^ 97))
 print('xor test 0bx11 is ', bin(97 ^ 98))
 
 
 
-euler59()
+# euler59()
 testStoA()
 testAtoS()
 testAtoB()
