e43f28bdfb4ccd8aeaf1cade6330723cbd06dbe1
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-13 17:08:59 +0800

    feat: extract helper method

--- src/euler62.py
+++ src/euler62.py
@@ -1,7 +1,6 @@
 from utils.annotations import track_performance
 from utils.myitertools import getPermsOfNumber
 import math
-import copy
 
 def testLog():
   print('2 is ', math.log(4,2))
@@ -22,38 +21,7 @@ def testCube():
 def euler62():
   print('project euler problem 62')
 
-def getPermutationsR(arr):
-  if len(arr) == 1:
-    return arr
-  if len(arr) == 2:
-    rev = copy.copy(arr)
-    rev.reverse()
-    return [arr, rev]
-  # isolate first item 
-  sub1 = arr[:1][0]
-  sub2 = arr[1:]
-  permutations = getPermutationsR(sub2)
-  expanded = []
-  for i in range(len(arr)):
-    for idx, p in enumerate(permutations):
-      c = copy.copy(p)
-      c.insert(i, sub1)
-      expanded.append(c)
-  return expanded
-
-  print('sub1 sub2 ', sub1, sub2)
-
-def testGetPermutationsR():
-  test1 = [1,2]
-  test2 = [5,6]
-  test3 = [1,2,3]
-  test4 = [1,2,3,4]
-  print('12 21 is ', getPermutationsR(test1))
-  print('56, 65 is ', getPermutationsR(test2))
-  print('123, 132, etc... is ', getPermutationsR(test3))
-  print('1234, 1243, etc... is ', getPermutationsR(test4))
 
 testLog()
 testCube()
 euler62()
-testGetPermutationsR()
--- src/utils/myitertools.py
+++ src/utils/myitertools.py
@@ -55,6 +55,35 @@ def getPermsOfNumber(num):
   asInts = list(map(lambda x: int("".join(x)), newArr))
   return asInts
 
+def getPermutationsR(arr):
+  if len(arr) == 1:
+    return arr
+  if len(arr) == 2:
+    rev = copy.copy(arr)
+    rev.reverse()
+    return [arr, rev]
+  # isolate first item 
+  sub1 = arr[:1][0]
+  sub2 = arr[1:]
+  permutations = getPermutationsR(sub2)
+  expanded = []
+  for i in range(len(arr)):
+    for idx, p in enumerate(permutations):
+      c = copy.copy(p)
+      c.insert(i, sub1)
+      expanded.append(c)
+  return expanded
+
+def testGetPermutationsR():
+  test1 = [1,2]
+  test2 = [5,6]
+  test3 = [1,2,3]
+  test4 = [1,2,3,4]
+  print('12 21 is ', getPermutationsR(test1))
+  print('56, 65 is ', getPermutationsR(test2))
+  print('123, 132, etc... is ', getPermutationsR(test3))
+  print('1234, 1243, etc... is ', getPermutationsR(test4))
+
 firstPerm = [1,2,3,4,5,6,7,8,9]
 
 def testPermutations():
@@ -70,3 +99,4 @@ def testGetPermsOfNumber():
 
 # testPermutations()
 # testGetPermsOfNumber()
+# testGetPermutationsR()
