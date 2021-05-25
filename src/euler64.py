93773925dde17c8d5cee7c33b4dea5b432c62a62
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-25 11:48:45 +0800

    feat: add a new way to calculate repeating patterns for euler 64

--- src/euler64.py
+++ src/euler64.py
@@ -29,14 +29,34 @@ def getPattern(num):
       if count > 1000:
           return repeatRemainders, pattern[1:], count
 
+def getNextIter(i, n, d):
+    newN = n * -1
+    newTop = sqrt(i) + newN
+    newD = (i - n ** 2) / d
+    whole = int( newTop / newD )
+    newN = newN - (newD * whole)
+    return newN, newD, whole
+
+def getResult(i):
+    repeats = []
+    wholes = []
+    w = int(sqrt(i))
+    d = 1
+    n = w * -1
+    n, d, w = getNextIter(i, n, d)
+    while (n, d) not in repeats:
+        repeats.append((n, d))
+        wholes.append(w)
+        n, d, w = getNextIter(i, n, d)
+    return repeats, wholes
 
 @track_performance
 def euler64():
     print('project euler problem 64')
     oddCount = 0
     for i in range(10001):
-        repeats, remainders, count = getPattern(i)
-        if oddPeriod(remainders):
+        repeats, wholes = getResult(i)
+        if oddPeriod(wholes):
             oddCount += 1
     print('oddCount is ', oddCount)
     
@@ -61,6 +81,12 @@ def testTupleComparison():
     print('False is ', t1 == t2)
     print('True is ', t1 == t3)
 
+def testGetNextIter():
+    n1, d1, w = getNextIter(43, -6, 1)
+    print('43, -1, 7 are ', n1, d1, w)
+    n2, d2, w2 = getNextIter(43, n1, d1)
+    print('43, -5, 6 are ', n2, d2, w2)
+
 def testTupleSearch():
     t1 = (5, 7)
     t2 = (7, -2)
@@ -70,8 +96,14 @@ def testTupleSearch():
     print('True is ', t3 in ta)
     print('False is ', t4 in ta)
 
-# euler64()
+def testResult():
+    repeats, wholes = getResult(43)
+    print('result of 43 is ', repeats, wholes)
+
+euler64()
 # testRemainder()
-testGetPattern()
-testTupleComparison()
-testTupleSearch()
+# testGetPattern()
+# testTupleComparison()
+# testTupleSearch()
+# testGetNextIter()
+# testResult()
