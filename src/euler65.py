a6106fb4c4d2340f481a1a50a163ef428f000f53
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-25 16:56:25 +0800

    feat: make series for euler 65

--- src/euler65.py
+++ src/euler65.py
@@ -1,7 +1,23 @@
 from utils.annotations import track_performance
 
+def makeFractionSeriesForE(lim):
+    res = [1]
+    for i in range(lim-1):
+        dividend = int(i / 3)
+        rem = i % 3
+        ev = 2 * (dividend + 1)
+        if rem == 0:
+            res.append(ev)
+        else:
+            res.append(1)
+    return res
+        
+def testSeries():
+    print('100 series is ', makeFractionSeriesForE(100))
+
 @track_performance
 def euler65():
     print('project euler problem 65')
     
 euler65()
+testSeries()
