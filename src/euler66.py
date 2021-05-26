85c39265c36566444ce01acdc9514cad456ef54f
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-26 16:38:42 +0800

    feat: add 100 to 200

--- src/euler66.py
+++ src/euler66.py
@@ -36,11 +36,11 @@ def testSolveX():
 def euler66():
     print('project euler problem 66')
     solutions = []
-    for i in range(101):
+    for i in range(101, 201):
         x = solveXDiophantine(i)
         solutions.append(x)
     print('solutions is ', solutions)
-    writeArrayToFile('./resources/diophantines0to100.txt', solutions)
+    writeArrayToFile('./resources/diophantines100to200.txt', solutions)
     
 euler66()
 # testIsPerfect()
