54506dff1164af182d8e7769a921dcf7aeb84e2d
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-01 11:29:00 +0800

    feat: start working on the 3gon problem

--- src/euler68.py
+++ src/euler68.py
@@ -1,5 +1,19 @@
 from utils.annotations import track_performance
+from itertools import permutations
 
+threeGon = [
+    [0,1,2],
+    [3,2,4],
+    [5,4,1]
+]
+
+oneToSix = list(range(1,7))
+print('oneToSix ', oneToSix)
+
+perms = list(map(lambda x: list(x), list(permutations(oneToSix))))
+
+
+print('perms are ', perms)
 
 @track_performance
 def euler68():
