a14065ff701345ab2847d4498514708db903a85d
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-01 10:35:16 +0800

    feat: euler 67: prepare the 2-dim array

--- src/euler67.py
+++ src/euler67.py
@@ -1,7 +1,11 @@
 from utils.annotations import track_performance
+from utils.fileUtils import openAndSplitPlus
 
 @track_performance
 def euler67():
+    triangleLines = openAndSplitPlus('./resources/p067_triangle.txt', '\n')
+    noEmpty = list(filter(lambda x: len(x) > 0, triangleLines))
+    asArr = list(map(lambda x: x.split(' '), noEmpty))
     print('project euler problem 67')
 
 
