2724baa95524e049759d89a44826cc4e68ad54ca
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-02 11:19:40 +0800

    feat: relative primes method

--- src/euler69.py
+++ src/euler69.py
@@ -1,13 +1,53 @@
 from utils.annotations import track_performance
+from utils.mathHelpers import getFactors, isPrime
+
+factorsMap = {}
+
+def getFactorsWithout1(n):
+    try:
+        f = factorsMap[n]
+        return f
+    except:
+        factors = getFactors(n)
+        factors.pop(0)
+        factorsMap[n] = factors
+        return factors
 
 
+def hasCommon(a,b):
+  for i in a:
+      if i in b:
+          return True
+  return False
 
 def getRelativePrimes(n):
+    if isPrime(n):
+        rps = list(range(n))
+        rps.pop(0)
+        return rps
+    factorsOfN = getFactorsWithout1(n)
+    factorsMap[n] = factorsOfN
+    rp = []
     for i in range(1,n):
-        if 
+        factorsOfI = getFactorsWithout1(i)
+        if not hasCommon(factorsOfI, factorsOfN):
+          rp.append(i)
+    return rp
+        
 
 @track_performance
 def euler69():
     print('project euler problem 69')
 
+def testFactors():
+    print('factors without 1 27', getFactorsWithout1(27))
+    print('factors without 1 42', getFactorsWithout1(42))
+    print('factors without 1 42', getFactorsWithout1(42))
+
+def testRp():
+    for i in range(2,11):
+        print('testRP i is ', i, getRelativePrimes(i))
+
 euler69()
+# testFactors()
+testRp()
