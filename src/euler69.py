ba2ad3ca4d74b37994b7e73a6abff44a054d34dc
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-02 16:56:06 +0800

    feat: attempt at improving perf

--- src/euler69.py
+++ src/euler69.py
@@ -60,20 +60,53 @@ def getRelativePrimes2(n):
             rps.append(key)
     return rps
 
+
+def processSeive(seive):
+    rps = [1]
+    for (key, val) in seive.items():
+      if val:
+          rps.append(key)
+    return rps
+
+def getRP(n):
+    seive = {}
+    primes = primeMap.keys()
+    if n in primes:
+        return list(range(1, n))
+    while True:
+        for pr in primes:
+            if pr >= n:
+                return processSeive(seive)
+            nonfactor = n%pr != 0 # nonfactor true means it is coprime
+            seive[pr] = nonfactor
+            for i in range(2, n//pr + 1):
+                j = i * pr
+                if j < n:
+                    try:
+                        se = seive[j]
+                        if not se or nonfactor:
+                            continue
+                        seive[j] = nonfactor
+                    except:
+                        seive[j] = nonfactor
+    return processSeive(seive)
+
 def getStats(n, rps):
     count = len(rps)
     return n / count
 
+
 @track_performance
 def euler69():
     print('project euler problem 69')
-    tmax = (11550,4.1825)
+    tmax = (11550, 4.1825)
     for i in range(20000, 100000):
-        cur = getStats(i, getRelativePrimes2(i))
+        cur = getStats(i, getRP(i))
         a, b = tmax
         if cur > b:
             tmax = i, cur
     print('tmax is ', tmax)
+
 
 def testFactors():
     print('factors without 1 27', getFactorsWithout1(27))
@@ -83,8 +116,7 @@ def testFactors():
 
 def testRp():
     for i in range(2, 11):
-        print('testRP i is ', i, getRelativePrimes2(i))
-        print('statss are ', i, getStats(i, getRelativePrimes(i)))
+        print('getRp i is ', i, getRP(i))
 
 
 euler69()
