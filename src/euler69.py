6b52055608eaa5181d256198c657262bae1951d8
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-02 12:05:28 +0800

    feat: try a new method with seive

--- src/euler69.py
+++ src/euler69.py
@@ -3,6 +3,7 @@ from utils.mathHelpers import getFactors, isPrime
 
 factorsMap = {}
 
+
 def getFactorsWithout1(n):
     try:
         f = factorsMap[n]
@@ -14,11 +15,12 @@ def getFactorsWithout1(n):
         return factors
 
 
-def hasCommon(a,b):
-  for i in a:
-      if i in b:
-          return True
-  return False
+def hasCommon(a, b):
+    for i in a:
+        if i in b:
+            return True
+    return False
+
 
 def getRelativePrimes(n):
     if isPrime(n):
@@ -28,28 +30,54 @@ def getRelativePrimes(n):
     factorsOfN = getFactorsWithout1(n)
     factorsMap[n] = factorsOfN
     rp = []
-    for i in range(1,n):
+    for i in range(1, n):
         factorsOfI = getFactorsWithout1(i)
         if not hasCommon(factorsOfI, factorsOfN):
-          rp.append(i)
+            rp.append(i)
     return rp
-        
+
+
+def getRelativePrimes2(n):
+    rps = [1]
+    seive = {}
+    for i in range(2, n):
+        # check seive first
+        try:
+            cur = seive[i]
+            continue
+        except:
+            divisible = n % i == 0
+            seive[i] = divisible
+            for j in range(i, n//i):
+                cur = i*j
+                try:
+                    s = seive[cur]
+                except:
+                    seive[cur] = divisible
+    for (key, val) in seive.items():
+        if not val:
+            rps.append(key)
+    return rps
+
 
 @track_performance
 def euler69():
     print('project euler problem 69')
-    for i in range(2,1000000):
+    for i in range(2, 1000000):
         getFactors(i)
+
 
 def testFactors():
     print('factors without 1 27', getFactorsWithout1(27))
     print('factors without 1 42', getFactorsWithout1(42))
     print('factors without 1 42', getFactorsWithout1(42))
 
-def testRp():
-    for i in range(2,11):
-        print('testRP i is ', i, getRelativePrimes(i))
 
-euler69()
+def testRp():
+    for i in range(2, 11):
+        print('testRP i is ', i, getRelativePrimes2(i))
+
+
+# euler69()
 # testFactors()
-# testRp()
+testRp()
