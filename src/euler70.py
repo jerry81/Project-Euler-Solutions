30e603f0c7986eb0df1b474100e7c48a0cd424cd
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-09 12:01:38 +0800

    feat: init euler 70

---
+++ src/euler70.py
@@ -0,0 +1,12 @@
+from utils.annotations import track_performance
+
+def arePerms(a,b):
+    pass
+
+# do primes' toitient values have any chance of being perm of the prime?
+
+@track_performance
+def euler70():
+    print('project euler problem 70')
+
+euler70()
