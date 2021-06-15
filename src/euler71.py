342e2fa6049b5e9c55c2a127c8cdf050c4ced8e6
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-15 13:33:50 +0800

    feat: solution for euler 70

--- src/euler71.py
+++ src/euler71.py
@@ -5,6 +5,22 @@ def getNForDCloseTo3over7(d):
 
 @track_performance
 def euler71():
+    minDiff = 1000000
+    candidate = 0
+    candidated = 0
     print('project euler problem 71')
+    for d in range(1, 1000001):
+        n = getNForDCloseTo3over7(d)
+        if (n == 3 and d == 7):
+            continue
+        dec = n / d
+        diff = (3/7) - dec
+        if diff == 0:
+            continue 
+        if diff < minDiff:
+            minDiff = diff 
+            candidate = n
+            candidated = d
+    print('n is ', candidate, candidated)
 
 euler71()
