a324fffdc95059dfd84f22a7f81ae113725f4a24
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-16 15:30:02 +0800

    feat: process down to 300k

--- src/euler72.py
+++ src/euler72.py
@@ -4,8 +4,8 @@ from utils.toitientHelpers import totient, o1isPrime
 @track_performance
 def euler72():
     print('project euler problem 72')
-    tot = 227972512716.0
-    for d in range(400001, 500001):
+    tot = 276606803908.0
+    for d in range(200001, 300001):
       if o1isPrime(d):
           tot += d - 1
           continue
