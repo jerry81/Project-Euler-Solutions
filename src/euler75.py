e66ed88af815fd7b9d4da4ae55e220efcdf2e266
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-21 11:33:04 +0800

    feat: helper tests for euler 75

--- src/euler75.py
+++ src/euler75.py
@@ -11,4 +11,17 @@ def isWhole(n):
 def euler75():
     print('project euler problem 75')
 
+def testIsWhole():
+    print('isWhole 7.0', isWhole(7.0))
+    print('isWhole 5/2', isWhole(5/2))
+    print('isWhole 6/2', isWhole(6/2))
+
+def testGetB():
+    print('getB 3 12', getBGivenAAndT(3, 12))
+    print('getB 2 12', getBGivenAAndT(2, 12))
+    print('getB 6, 24', getBGivenAAndT(6,24))
+    print('getB 20, 120', getBGivenAAndT(20, 120))
+
 euler75()
+# testIsWhole()
+testGetB()
