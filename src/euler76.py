748757fc1000bf5e565acfb4ae33857d8c4b5bc4
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-10 17:12:17 +0800

    feat: can't see recursive pattern yet

--- src/euler76.py
+++ src/euler76.py
@@ -1,6 +1,28 @@
 from utils.annotations import track_performance
 
 # sums of 5
+# 1,4
+# 2,3
+# 1, sums of 4 - 1,3,1
+  # 1, 2,2
+# 2, sums of 3 - 2,1,1,1 
+     # 2, 2, 1 - repeats with sums of 4
+# 3, sums of 2 - 3,1,1 - repeats with sums of 4
+
+# sums of 4
+# 1, sums of 3 - 1,1,1
+   # 1,1,2
+# 2, sums of 2
+  # 2, 1, 1 - repeat with sums of 3
+# 3, 1
+# 2, 2
+
+# sums of 3
+# 1, sums of 2 - 1,1,1
+# 2, 1 
+
+# sums of 2
+# 1,1
 
 @track_performance
 def euler76():
